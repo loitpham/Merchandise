7170c0b08a4d6676d665c42c6ea2d69e7970dbd2
Loi Pham <loitpham@gmail.com>
2021-04-08 21:53:28 -0700

    Added a custom Navigation Bar that can handle the presense/absence of notch at the top

---
+++ Merchandise/Merchandise/View/Home/LogoView.swift
@@ -0,0 +1,36 @@
+//
+//  LogoView.swift
+//  Merchandise
+//
+//  Created by Loi Pham on 4/8/21.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        } //: HSTACK
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
