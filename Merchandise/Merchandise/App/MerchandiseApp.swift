dac3c0f21d6a65e4426920be1bf628a71766abe5
Loi Pham <loitpham@gmail.com>
2021-04-08 21:15:07 -0700

    Added .gitignore

---
+++ Merchandise/Merchandise/App/MerchandiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MerchandiseApp.swift
+//  Merchandise
+//
+//  Created by Loi Pham on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct MerchandiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Merchandise/Merchandise/Utility/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  Merchandise
+//
+//  Created by Loi Pham on 4/8/21.
+//
+
+import Foundation
