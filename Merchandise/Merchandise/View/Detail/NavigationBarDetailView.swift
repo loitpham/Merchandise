f022c3a773e7758e482a999bb2d78d8c217d9d08
Loi Pham <loitpham@gmail.com>
2021-04-09 23:59:02 -0700

    Fixed the back button on the product detail page

--- Merchandise/Merchandise/View/Detail/NavigationBarDetailView.swift
+++ Merchandise/Merchandise/View/Detail/NavigationBarDetailView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct NavigationBarDetailView: View {
     // MARK: - PROPERTIES
-    
+    @EnvironmentObject var shop: Shop
     
     // MARK: - BODY
     
     var body: some View {
         HStack {
-            Button(action: {}) {
+            Button(action: {
+                withAnimation(.easeIn) {
+                    shop.selectedProduct = nil
+                    shop.showingProduct = false
+                }
+            }) {
                 Image(systemName: "chevron.left")
                     .font(.title)
                     .foregroundColor(.white)
@@ -37,6 +42,7 @@ struct NavigationBarDetailView: View {
 struct NavigationBarDetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationBarDetailView()
+            .environmentObject(Shop())
             .previewLayout(.sizeThatFits)
             .padding()
             .background(Color.gray)
